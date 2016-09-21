f253cf50e704345de55b9345edfe00646f527ef2
AlexanderMann <alex@circleci.com>
2016-09-21 14:53:43 -0700

    autocompletion for git

--- git/install.sh
+++ git/install.sh
@@ -1,3 +1,7 @@
 touch ~/.gitignore_global;
 rm ~/.gitignore_global;
 cp .gitignore_global ~;
+
+touch ~/.git-completion.bash
+rm ~/.git-completion.bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
