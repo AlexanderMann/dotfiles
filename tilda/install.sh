3bceb9a0dd95571c61d4411d99d042d5e2a082bc
AlexanderMann <alex@circleci.com>
2016-10-26 15:40:54 -0400

    brewfile

--- tilda/install.sh
+++ tilda/install.sh
@@ -9,6 +9,12 @@ touch ~/.bash_profile;
 rm ~/.bash_profile;
 cp .bash_profile ~;
 
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update;
+brew tap Homebrew/bundle;
+brew bundle;
+
 touch ~/.lein/profiles.clj;
 rm ~/.lein/profiles.clj;
 cp .lein/profiles.clj ~/.lein/profiles.clj;
