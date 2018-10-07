6ceff5162c24f09c5c0fdf44d71741ae2cc328e3
AlexanderMann <mannimal09@gmail.com>
2018-10-07 11:46:03 -0500

    addin auto setup for git

--- git/install.sh
+++ git/install.sh
@@ -3,7 +3,10 @@ set -x
 touch ~/.gitignore_global;
 rm ~/.gitignore_global;
 cp .gitignore_global ~;
+
 git config --global core.excludesfile ~/.gitignore_global;
+git config --global user.name "AlexanderMann"
+git config --global user.email mannimal09@gmail.com
 
 touch ~/.git-completion.bash
 rm ~/.git-completion.bash
